916841a07f5952cf7efea97512a9e3e6af3d150b
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-08-03 11:30:43 -0700

    Why

--- index.js
+++ index.js
@@ -1,84 +1,105 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
+const inquirer = require("inquirer")
+const fs = require("fs")
 
-const manager = inquirer.prompt([
-    {
-        name: "managerName",
-        message: "What is the name of the manager for this project?",
-        type: "input",
-    },
-    {
-        name: "id",
-        message: "What is the managers ID #?",
-        type: "input",
-    },
-    {
-        name: "email",
-        message: "What is the managers email?",
-        type: "input",
 
-    },
-    {
-        name: "office",
-        message: "What is the office number for the manager?",
-        type: "input",
-    },
-])
 
-const engineer = inquirer.prompt([
-    {
-        name: "engineerName",
-        message: "What is the selected engineers name?",
-        type: "input",
-    },
-    {
-        name: "engineerId",
-        message: "What is the selected engineers id number?",
-        type: "input",
-    },
-    {
-        name: "engineerEmail",
-        message: "What is the email for the selected engineer?",
-        type: "input",
-    },
-    {
-        name: "engineerGithub",
-        message: "What is the github link for the selected engineer?",
-        type: "input",
-    },
-])
+function manager() {
+    inquirer.prompt([
+        {
+            name: "managerName",
+            message: "What is the name of the manager for this project?",
+            type: "input",
+        },
+        {
+            name: "id",
+            message: "What is the managers ID #?",
+            type: "input",
+        },
+        {
+            name: "email",
+            message: "What is the managers email?",
+            type: "input",
 
-const intern = inquirer.prompt([
-    {
-        name: "internName",
-        message: "What is the name of the selected intern?",
-        type: "input",
-    },
-    {
-        name: "internId",
-        message: "What is the selected interns Id #?",
-        type: "input",
-    },
-    {
-        name: "internEmail",
-        message: "What is the email of the selected intern?",
-        type: "input",
-    },
-    {
-        name: "internSchool",
-        message: "What is the name of the school the intern attends?",
-        type: "input",
-    }
-])
+        },
+        {
+            name: "office",
+            message: "What is the office number for the manager?",
+            type: "input",
+        },
+    ])
+};
 
-const addTeam = inquirer.prompt([
-    {
-        name: "nextMember",
-        message: "Would you like to add another team member? If yes select new member.",
-        type: "list",
-        choices: ['Engineer', 'Intern'],
-    },
-])
+manager();
+
+function engineer() {
+    inquirer.prompt([
+        {
+            name: "engineerName",
+            message: "What is the selected engineers name?",
+            type: "input",
+        },
+        {
+            name: "engineerId",
+            message: "What is the selected engineers id number?",
+            type: "input",
+        },
+        {
+            name: "engineerEmail",
+            message: "What is the email for the selected engineer?",
+            type: "input",
+        },
+        {
+            name: "engineerGithub",
+            message: "What is the github link for the selected engineer?",
+            type: "input",
+        },
+    ])
+};
+
+engineer();
+
+
+function intern() {
+    inquirer.prompt([
+        {
+            name: "internName",
+            message: "What is the name of the selected intern?",
+            type: "input",
+        },
+        {
+            name: "internId",
+            message: "What is the selected interns Id #?",
+            type: "input",
+        },
+        {
+            name: "internEmail",
+            message: "What is the email of the selected intern?",
+            type: "input",
+        },
+        {
+            name: "internSchool",
+            message: "What is the name of the school the intern attends?",
+            type: "input",
+        }
+    ])
+};
+
+intern();
+
+
+
+function newMember() {
+    inquirer.prompt([
+        {
+            name: "nextMember",
+            message: "Would you like to add another team member? If yes select new member.",
+            type: "list",
+            choices: ['Engineer', 'Intern', 'Done'],
+        },
+    ])
+};
+
+newMember();
 
 
 // Ask what type of team member to be added or complete.
